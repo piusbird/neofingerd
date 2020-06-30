6a1feedde7751bdfacb6caea508c67c83d7adb71
Matt Arnold <matt@thegnuguru.org>
2020-06-29 22:00:51 -0400

    Add screenshot

--- neofingerd.sh
+++ neofingerd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 . .env
-$NF_PREPEND tcpserver -v -u $NF_RUNAS -g $NF_GROUP  0 $NF_PORT $NF_BASEDIR/neofinger.py
+$NF_PREPEND tcpserver -v -u $NF_RUNAS -g $NF_GROUP  0 $NF_PORT $NF_BASEDIR/finger-wrapper.sh
